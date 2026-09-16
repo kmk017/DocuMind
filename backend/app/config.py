34f52f0f9266cc1adbf5e1b34d70ab263c0e44a8
Mohan Kumar K <thekmk017@gmail.com>
2026-09-16 14:18:25 +0530

    enhanced config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,6 +2,14 @@ import os
 
 
 class Config:
+
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 1800,
+    }
+
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024  # 20 MB — requirement 7
